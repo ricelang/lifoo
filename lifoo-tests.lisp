0ebb25f4a4664d72dcce2bb0eb8b9917e84e433b
codr4life <codr4life@gmail.com>
2017-01-31 20:33:25 +0100

    added copy-env

--- lifoo-tests.lisp
+++ lifoo-tests.lisp
@@ -73,7 +73,7 @@
 (define-test (:lifoo :env)
   (assert (= 42 (do-lifoo ()
                   :foo 42 set drop :foo get)))
-  (assert (equal '((:foo . 42) (:bar . 7))
+  (assert (equal '((:bar . 7) (:foo . 42))
                  (do-lifoo ()
                    :foo 42 set :bar 7 set env)))
   (assert (equal '(nil . 42)
