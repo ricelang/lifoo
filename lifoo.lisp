cde8fd4db84e314fd11a1bc547b3c89efc0ebe74
codr4life <codr4life@gmail.com>
2017-02-02 00:19:56 +0100

    todo

--- lifoo.lisp
+++ lifoo.lisp
@@ -454,7 +454,7 @@
 (define-lifoo-init init-stack
   ;; Pushes stack on stack as list
   (define-lisp-word :stack ()
-    (lifoo-push (copy-list (stack *lifoo*))))
+    (lifoo-push (stack *lifoo*)))
   
   ;; Pops stack
   (define-lisp-word :drop ()
